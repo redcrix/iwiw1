97ec7b90bef1822ba35128b951a896d5afc6ddca
redcrix <contact@redcrix.com>
2019-08-30 15:11:08 +0530

    tested

--- src/components/components.module.ts
+++ src/components/components.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CategoriesComponent } from './categories/categories';
 import { ScrollingFeaturedProductsComponent } from './scrolling-featured-products/scrolling-featured-products';
+import { IconsComponent } from './icons/icons';
 // import { BannersComponent } from './banners/banners';
 // import { ProductComponent } from './product/product';
 // import { FooterComponent } from './footer/footer';
 // import { SlidingTabsComponent } from './sliding-tabs/sliding-tabs';
 // import { HeaderComponent } from './header/header';
 @NgModule({
-    declarations: [
+    declarations: [IconsComponent,
+    IconsComponent
         // CategoriesComponent,
         // ScrollingFeaturedProductsComponent
         // BannersComponent,
@@ -17,7 +19,8 @@ import { ScrollingFeaturedProductsComponent } from './scrolling-featured-product
         // HeaderComponent,
     ],
     imports: [],
-    exports: [
+    exports: [IconsComponent,
+    IconsComponent
         // CategoriesComponent,
         // ScrollingFeaturedProductsComponent
         // BannersComponent,
